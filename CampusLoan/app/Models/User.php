03dbcadcb575adf39da94c8241f9847a1ce8516f
Muhamad ali pasha <pasyama865@gmail.com>
2026-01-29 03:20:50 +0700

    perubahan

--- CampusLoan/app/Models/User.php
+++ CampusLoan/app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -22,6 +22,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
